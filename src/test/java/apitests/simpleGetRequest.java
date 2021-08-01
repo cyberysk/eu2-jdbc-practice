8999aa72e923fbc2121c2ed183ba1ee5d5855f9c
cyberysk <yskcybertest@gmail.com>
2021-08-01 13:02:22 +0200

    update simple.java

--- src/test/java/apitests/simpleGetRequest.java
+++ src/test/java/apitests/simpleGetRequest.java
@@ -1,5 +1,6 @@
 package apitests;
 
+import static io.restassured.RestAssured.*;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
@@ -40,8 +41,38 @@ public class simpleGetRequest {
         //verify content type is application/json
         Assert.assertEquals(response.contentType(), "application/json");
 
+    }
 
+    @Test
+    public void test3(){
 
+        RestAssured.given().accept(ContentType.JSON)
+                .when().get(hrurl).then()
+                .assertThat().statusCode(200)
+                .and().contentType("application/json");
 
     }
+    /*  Given accept type is json
+        When the user sends get request to regions/2
+        Then response status code must be 200
+        and body is json format
+        and response body contains Americas
+        */
+
+    @Test
+    public void test4(){
+
+        Response response = given().accept(ContentType.JSON)
+                            .when().get(hrurl+"\2");
+
+        //verify status code
+        Assert.assertEquals(response.statusCode(), 200);
+
+        //verify content type
+        Assert.assertEquals(response.contentType(),"application/json");
+
+        //verify body includes Americas
+        Assert.assertTrue(response.body().asString().contains("Americas"));
+    }
+
 }
